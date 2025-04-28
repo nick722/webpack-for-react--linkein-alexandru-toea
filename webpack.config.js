160db7634ae49796dcbab6743fa6ae3c7ababe03
Nikolai <nick22783@gmail.com>
2025-04-28 16:35:39 +0300

    works

--- webpack.config.js
+++ webpack.config.js
@@ -5,18 +5,18 @@
 //   }
 // }
 
-var webpack = require('webpack');
-var path = require('path');
-var htmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require("webpack")
+var path = require("path")
+var HtmlWebpackPlugin = require("html-webpack-plugin")
 
-var BUILD_DIR = path.join(__dirname, 'dist');
-var APP_DIR = path.join(__dirname, 'src');
+var BUILD_DIR = path.join(__dirname, "dist")
+var APP_DIR = path.join(__dirname, "src")
 
 var config = {
-  entry: APP_DIR + '/index.js',
+  entry: APP_DIR + "/index.js",
   output: {
     path: BUILD_DIR,
-    filename: 'app.bundle.js'
+    filename: "app.bundle.js",
   },
   module: {
     rules: [
@@ -24,27 +24,27 @@ var config = {
         test: /\.(js|jsx)$/,
         // include: APP_DIR,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: "babel-loader",
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
-        use: 'file-loader'
-      }
-    ]
+        use: "file-loader",
+      },
+    ],
   },
   plugins: [
-    new htmlWebpackPlugin({
-      template: 'index.html'
-    })
-  ]
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+  ],
 }
 
-module.exports = config;
+module.exports = config
